9bb90780c3449b5cff9eaa19624a2daeb7bbc502
NathaN158 <106011846@student.swin.edu.au>
2025-04-14 12:54:28 +1000

    maybe?

--- myfirst.php
+++ myfirst.php
@@ -7,14 +7,14 @@
         <meta name="keywords" content="HTML, PHP, Array, Variables, operators">
         <meta name="author" content="Nathan Nguyen">
 </head>
-<body?>
+<body>
     <h1>PHP Variables, arrays and operators </h1>
 
 <?php 
     $days = array(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
 
     echo "<p> The days of the week in English are: $days. </p>"
-    ?>
+?>
 <?php
     $marks = array (85, 85, 95);
     $marks[1] = 90;
