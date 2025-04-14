b78f6b0010d26e0b83a854dc5bbd545614afbd05
NathaN158 <106011846@student.swin.edu.au>
2025-04-14 12:56:57 +1000

    Yes?

--- myfirst.php
+++ myfirst.php
@@ -11,19 +11,9 @@
     <h1>PHP Variables, arrays and operators </h1>
 
 <?php 
-    $days = array(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
+    $days = array(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday);
 
     echo "<p> The days of the week in English are: $days. </p>";
-?>
-<?php
-    $marks = array (85, 85, 95);
-    $marks[1] = 90;
-    $ave = ($marks[0] + $marks[1] +$marks[2])/3;
-    if ($ave >= 50)
-        $status = "PASSED";
-    else
-        $status = "FAILED";
-    echo "<p>The average score is $ave. You $status.</p>";
 ?>
 </body>
 </html>
