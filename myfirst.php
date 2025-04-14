7bb0a3fc5872ada775c3a7bd483fa9d3ebbab2b4
NathaN158 <106011846@student.swin.edu.au>
2025-04-14 13:05:41 +1000

    uh huh?

--- myfirst.php
+++ myfirst.php
@@ -12,8 +12,7 @@
 
 <?php 
     $days = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-
-    echo "<p> The days of the week in English are: $days[0] </p>";
+    echo "<p> The days of the week in English are: $days[0], $days[1], $days[2], $days[3], $days[4], $days[5], $days[6] </p>";
 ?>
 </body>
 </html>
