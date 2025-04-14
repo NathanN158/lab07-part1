409410b2e5f783bbfc695e87e18b1e26763f060f
NathaN158 <106011846@student.swin.edu.au>
2025-04-14 12:40:56 +1000

    code added

---
+++ myfirst.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title> Using POHP Vaiables, arrays and operators</title>
+        <meta name="description" content="Introduction to PHP elements">
+        <meta name="keywords" content="HTML, PHP, Array, Variables, operators">
+        <meta name="author" content="Nathan Nguyen">
+</head>
+<body?>
+    <h1>PHP Variables, arrays and operators </h1>
+<?php
+    $marks = array (85, 85, 95);
+    $marks[1] = 90;
+    $ave = ($marks[0] + $marks[1] +$marks[2])/3;
+    if ($ave >= 50)
+        $status = "PASSED";
+    else
+        $status = "FAILED";
+    echo "<p>The average score is $ave. You $status.</p>";
+?>
+</body>
+</html>
